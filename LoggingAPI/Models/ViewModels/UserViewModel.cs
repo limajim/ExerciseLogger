cf3656aa70faec722b2b0f32fcbbb02dd81072bd
jvandickcscos <jvandick@cscos.com>
2016-03-21 09:48:36 -0400

    Forgot to add the new files

---
+++ LoggingAPI/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace LoggingAPI.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public List<Permission> Permissions { get; set; }
+    }
+}
